d4c9be41a9d46cca10c0a9c2c2cadc5190c4a99f
aviralgarg05 <gargaviral99@gmail.com>
2026-01-01 12:52:06 +0530

    Revert "fix: resolve hindi button unclickability and hide google banner"
    This reverts commit 98d22d5623c401ac86a5343763ccfa09600a8a76.

--- components/GoogleTranslate.tsx
+++ components/GoogleTranslate.tsx
@@ -3,85 +3,69 @@ import { useEffect } from 'react';
 import Script from 'next/script';
 
 declare global {
-  interface Window {
-    googleTranslateElementInit?: () => void;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    google?: any;
-  }
+    interface Window {
+        googleTranslateElementInit?: () => void;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        google?: any;
+    }
 }
 
 const GoogleTranslate = () => {
-  useEffect(() => {
-    window.googleTranslateElementInit = () => {
-      if (window.google && window.google.translate) {
-        new window.google.translate.TranslateElement(
-          {
-            pageLanguage: 'en',
-            includedLanguages: 'en,hi',
-            autoDisplay: false,
-            layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
-          },
-          'google_translate_element'
-        );
-      }
-    };
-  }, []);
+    useEffect(() => {
+        window.googleTranslateElementInit = () => {
+            if (window.google && window.google.translate) {
+                new window.google.translate.TranslateElement(
+                    {
+                        pageLanguage: 'en',
+                        includedLanguages: 'en,hi',
+                        autoDisplay: false,
+                        layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+                    },
+                    'google_translate_element'
+                );
+            }
+        };
+    }, []);
 
-  return (
-    <>
-      <div id="google_translate_element" style={{ display: 'none' }} />
-      <Script
-        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-        strategy="afterInteractive"
-      />
-      <style jsx global>{`
-        /* Aggressively hide Google Translate top bar & banner */
-        .goog-te-banner-frame,
-        .skiptranslate.goog-te-gadget-simple,
-        .vi-smart-banner,
-        .VIpgJd-ZVi9od-ORHb-OEVmcd,
-        #goog-gt-tt,
-        iframe.goog-te-banner-frame {
+    return (
+        <>
+            <div id="google_translate_element" style={{ display: 'none' }} />
+            <Script
+                src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+                strategy="afterInteractive"
+            />
+            <style jsx global>{`
+        /* Hide Google Translate top bar */
+        .goog-te-banner-frame.skiptranslate {
           display: none !important;
-          visibility: hidden !important;
-          height: 0 !important;
         }
-        
         body {
           top: 0px !important;
-          position: relative !important;
         }
-
         /* Hide "Suggest an edit" tooltip */
-        .goog-tooltip,
-        #goog-gt-tt {
+        .goog-tooltip {
           display: none !important;
-          visibility: hidden !important;
         }
-
         .goog-tooltip:hover {
           display: none !important;
         }
-
         .goog-text-highlight {
           background-color: transparent !important;
           border: none !important;
           box-shadow: none !important;
         }
-
         /* Hide the google translate element if somehow visible */
         #google_translate_element {
-          display: none !important;
+          display: none;
         }
-
         /* Fix google transform messing with font sizes */
         font {
           background-color: transparent !important;
           box-shadow: none !important;
         }
       `}</style>
-    </>
-  );
+        </>
+    );
 };
 
 export default GoogleTranslate;
