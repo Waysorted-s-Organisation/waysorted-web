eccf43cf41b6ff63804f48073f05c5fc65230855
aviralgarg05 <gargaviral99@gmail.com>
2026-01-08 18:17:36 +0530

    chore: restore splash duration to 4000ms (original speed)

--- app/layout.tsx
+++ app/layout.tsx
@@ -293,7 +293,7 @@ export default function RootLayout({
         />
       </head>
       <body className={`${hanken.className} no-scrollbar select-none`}>
-        <SplashGate minMs={1500} initialOnly>
+        <SplashGate minMs={4000} initialOnly>
           <BannerProvider>
             {children}
             {/* <EarlyAccessPopup /> */}
